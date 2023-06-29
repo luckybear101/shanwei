8b45737b4b6fc25536fabcd6e8b2587548ff094e
alex <calife@tutamail.com>
2023-06-29 13:57:48 -0400

    change the CTA bg-color

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 export default function CTA() {
   return (
-    <div className="bg-pink-100">
+    <div className="bg-gradient-to-t from-white via-fuchsia-200 to-white">
       <div className="py-24 px-6 sm:px-6 sm:py-32 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
