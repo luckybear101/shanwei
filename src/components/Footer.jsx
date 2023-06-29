0dac3fc177309fbd12a06549b5b4e6c632198398
alex <calife@tutamail.com>
2023-06-29 14:35:36 -0400

    footer copy rights

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2023 善为（北京）健康管理有限公司, Inc. All rights reserved.
+            &copy; 2023 善为（北京）健康管理有限公司, 版权所有.
           </p>
         </div>
       </div>
