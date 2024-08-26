a6583a14c559410d189c54234a659941653ceb09
alex <calife@tutamail.com>
2024-08-25 21:45:06 -0400

    Link to about

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -43,7 +43,7 @@ export default function CTA() {
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
-              to="/clients"
+              to="/about"
               className="inline-flex rounded-md bg-lime-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600"
             >
               了解更多
